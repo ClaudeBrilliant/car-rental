96bfaadbfb7d869c9056a9cfc0b4bb04bf45efaf
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-27 17:29:35 +0300

    added notification model

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { CreateNotificationDto } from './dto/notification.dto';
+import { NotificationService } from './notifications.service';
 
 @Controller('notifications')
-export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+export class NotificationController {
+  constructor(private readonly service: NotificationService) {}
+
+  @Post()
+  create(@Body() dto: CreateNotificationDto) {
+    return this.service.create(dto);
+  }
+
+  @Get('user/:userId')
+  findAllByUser(@Param('userId') userId: string) {
+    return this.service.findAllByUser(userId);
+  }
+
+  @Patch('read/:id')
+  markAsRead(@Param('id') id: string) {
+    return this.service.markAsRead(id);
+  }
+
+  @Patch('read-all/:userId')
+  markAllAsRead(@Param('userId') userId: string) {
+    return this.service.markAllAsRead(userId);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.service.delete(id);
+  }
 }
--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { NotificationsController } from './notifications.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { NotificationController } from './notifications.controller';
+import { NotificationService } from './notifications.service';
 
 @Module({
-  controllers: [NotificationsController],
-  providers: [NotificationsService],
+  controllers: [NotificationController],
+  providers: [NotificationService, PrismaService],
 })
-export class NotificationsModule {}
+export class NotificationModule {}
