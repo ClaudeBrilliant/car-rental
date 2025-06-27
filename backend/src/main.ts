fc0818fe906af5ec5bf3e7d87d0dd54bf3db659a
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-27 17:32:23 +0300

    changed main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle(' Car Rental API')
+    .setDescription('API documentation for the car rental system')
+    .setVersion('1.0')
+    .addBearerAuth() // Adds JWT bearer token auth
+    .build();
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
