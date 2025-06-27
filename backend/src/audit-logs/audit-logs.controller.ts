83d9c3bb253cd2d62b1e771b2dbc1ef6673f9e2d
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-27 17:28:49 +0300

    added audit log model

--- backend/src/audit-logs/audit-logs.controller.ts
+++ backend/src/audit-logs/audit-logs.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
-import { AuditLogsService } from './audit-logs.service';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { AuditLogService } from './audit-logs.service';
+import { CreateAuditLogDto } from './dto/create-audit.dto';
 
 @Controller('audit-logs')
-export class AuditLogsController {
-  constructor(private readonly auditLogsService: AuditLogsService) {}
+export class AuditLogController {
+  constructor(private readonly auditLogService: AuditLogService) {}
+
+  @Post()
+  create(@Body() createAuditLogDto: CreateAuditLogDto) {
+    return this.auditLogService.create(createAuditLogDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.auditLogService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.auditLogService.findOne(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.auditLogService.delete(id);
+  }
 }
--- backend/src/audit-logs/audit-logs.module.ts
+++ backend/src/audit-logs/audit-logs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuditLogsService } from './audit-logs.service';
-import { AuditLogsController } from './audit-logs.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuditLogController } from './audit-logs.controller';
+import { AuditLogService } from './audit-logs.service';
 
 @Module({
-  controllers: [AuditLogsController],
-  providers: [AuditLogsService],
+  controllers: [AuditLogController],
+  providers: [AuditLogService, PrismaService],
 })
-export class AuditLogsModule {}
+export class AuditLogModule {}
