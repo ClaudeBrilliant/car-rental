a0857a59e79c80dc86629c180ec79326e2d9b5c0
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-07-03 10:05:10 +0300

    added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,37 +1,39 @@
 import { Routes } from '@angular/router';
-import { HotelDashboard } from './hotel-dashboard/hotel-dashboard';
-import { BookingManagement } from './components/booking-management/booking-management';
+import { VehicleDashboardComponent } from './vehicle-dashboard/vehicle-dashboard';
 import { Rooms } from './components/rooms/rooms';
 import { RoomList } from './components/room-list/room-list';
 import { Login } from './components/login/login';
 import { AuthGuard } from './guards/auth-guard';
 import { UnauthorizedComponent } from './components/unauthorized/unauthorized';
 import { RoleGuard } from './guards/role-guard';
+import { VehicleBookingManagementComponent } from './components/booking-management/booking-management';
+import { VehicleComponent } from './components/vehicle/vehicle';
+import { VehicleList } from './components/vehicle-list/vehicle-list';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     // canActivate: [AuthGuard, RoleGuard],
     // data: { roles: ['ADMIN'] },
-    component: HotelDashboard,
+    component: VehicleDashboardComponent,
   },
   {
     path: 'bookings',
     // canActivate: [AuthGuard, RoleGuard],
-    // data: { roles: ['STAFF', 'ADMIN'] },
-    component: BookingManagement,
+    // data: { roles: ['AGENT' , 'ADMIN'] },
+    component: VehicleBookingManagementComponent,
   },
   {
-    path: 'rooms',
+    path: 'vehicles',
     // canActivate: [AuthGuard, RoleGuard],
-    // data: { roles: ['GUEST', 'STAFF', 'ADMIN'] },
-    component: Rooms,
+    // data: { roles: ['CUSTOMER', 'AGENT', 'ADMIN'] },
+    component: VehicleComponent,
   },
   {
-    path: 'room/list',
+    path: 'vehicles/list',
     // canActivate: [AuthGuard, RoleGuard],
-    // data: { roles: ['GUEST', 'STAFF', 'ADMIN'] },
-    component: RoomList,
+    // data: { roles: ['CUSTOMER', 'AGENT', 'ADMIN'] },
+    component: VehicleList,
   },
   { path: 'login', component: Login },
   { path: 'unauthorized', component: UnauthorizedComponent },
